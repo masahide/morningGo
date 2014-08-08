38ffed34ac79e39c95827686f34a121710cf44a9
YAMASAKI masahide <yamasaki_masahide@cygames.co.jp>
2014-08-08 13:47:21 +0900

    ファイル名を変更。 冗長なコードを整理

--- 20140731/hard/20140731-hard.go
+++ 20140731/hard/20140731-hard.go
@@ -6,6 +6,11 @@ import (
 	"github.com/kr/pretty"
 )
 
+func printIP(name string, ip *net.IP) {
+	if !ip.IsLoopback() {
+		pretty.Printf("%s:%s\n", name, ip)
+	}
+}
 func main() {
 	/* Hard:自分のマシンのIPアドレスを取得し画面表示 */
 	interfaces, _ := net.Interfaces()
@@ -18,19 +23,13 @@ func main() {
 			default:
 				pretty.Printf("unexpected type %t\n", ip)
 			case *net.IPNet:
-				if !ip.IP.IsLoopback() {
-					pretty.Printf("%s:%s\n", i.Name, ip)
-				}
+				printIP(i.Name, &ip.IP)
 			case *net.IPAddr:
-				if !ip.IP.IsLoopback() {
-					pretty.Printf("%s:%s\n", i.Name, ip)
-				}
+				printIP(i.Name, &ip.IP)
 				/*
 					//↓の書き方はコンパイルエラーに
 					case *net.IPNet, *net.IPAddr:
-						if !ip.IP.IsLoopback() {  // > ip.IP undefined (type net.Addr has no field or method IP)
-							pretty.Printf("%s:%s\n", i.Name, ip)
-						}
+						printIP(i.Name, &ip.IP)  // > ip.IP undefined (type net.Addr has no field or method IP)
 				*/
 			}
 		}
